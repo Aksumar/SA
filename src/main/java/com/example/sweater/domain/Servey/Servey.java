63fcc27b2ba613a7ff560de84b60058f92c28870
Lilia <iskhakovalilia@yandex.ru>
2019-02-20 22:57:25 +0300

    Add servey binding with user

--- src/main/java/com/example/sweater/domain/Servey/Servey.java
+++ src/main/java/com/example/sweater/domain/Servey/Servey.java
@@ -1,20 +1,34 @@
 package com.example.sweater.domain.Servey;
 
+import com.example.sweater.domain.User.User;
+
+import java.io.IOException;
 import java.util.List;
 
+/**
+ * Объект самой анкеты
+ */
 public class Servey {
+    User userUploader;
     /**
      * Объект-таблицы, пришедший на анализ.
      */
-    TableExcel tableToAnalize;
+    private TableExcel tableToAnalize;
     /**
      * В данном лсите хранятся номера важных для пользователя вопросов.
      * По данным вопросам будет запущен полный анализ
      */
-    List<Integer> importantQuestions;
+    private List<Integer> importantQuestions;
 
-    List<GroupOfQuestions> groupOfQuestions;
+    private List<GroupOfQuestions> groupOfQuestions;
 
-    public void GenerateResult(){}
+    private List<Term> terms;
+
+    public Servey(String pathToExcelFile, User user) throws IOException {
+        tableToAnalize = new TableExcel(pathToExcelFile, user);
+    }
+
+    public void GenerateResult() {
+    }
 
 }
