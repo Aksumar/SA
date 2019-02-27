cf63586e54c7ac41c4f061df0f93b3521125ab2c
Andrew <andpuc23@gmail.com>
2019-02-27 23:10:14 +0300

    started Group.speak()

--- src/main/java/com/example/sweater/Algorithms/descriptionGenerator/Group.java
+++ src/main/java/com/example/sweater/Algorithms/descriptionGenerator/Group.java
@@ -1,5 +1,6 @@
 package com.example.sweater.Algorithms.descriptionGenerator;
 
+import com.example.sweater.Algorithms.ExcelExtract.Question;
 import com.example.sweater.Algorithms.ExcelExtract.Responder;
 
 import java.util.ArrayList;
@@ -10,5 +11,15 @@ public class Group{
     public Group(String name, ArrayList<Responder> responders){
         this.name = name;
         people.addAll(responders);
+    }
+
+    public String speak (Question q){
+        StringBuilder output = new StringBuilder("На вопрос \"");
+        output.append(q.description);
+        output.append("\" группа" + this.name);
+        output.append("ЦЕ ЗАТЫЧКО\n");
+
+
+        return output.toString();
     }
 }
