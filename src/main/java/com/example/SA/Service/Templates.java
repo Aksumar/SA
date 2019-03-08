04fd296d1ab17fd7663ac318f1154be2eff2b818
paashikhmin <vc2afc1r>
2019-03-08 15:26:49 +0300

    now template loads from file. #unstable

--- src/main/java/com/example/SA/Service/Templates.java
+++ src/main/java/com/example/SA/Service/Templates.java
@@ -1,6 +1,7 @@
 package com.example.SA.Service;
 
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class Templates {
     public enum Type {
@@ -10,20 +11,54 @@ public class Templates {
         MIN
     }
 
-    private static final Random rnd = new Random();
+    ////////////////////////////////
+    private static final int
+        ACTIONS = 0,
+        ACTORS = 1,
+        LRATE = 2,
+        HRATE = 3,
+        DESCALL = 4,
+        MIDDLE = 5;
+    private TemplateBrick[] templates = new TemplateBrick[6];
+    ////////////////////////////////
 
-    private Templates() {
+    private static final List<String> _brickNames =
+            Arrays.asList(
+                    "actions",
+                    "actors",
+                    "lowRate",
+                    "highRate",
+                    "describeAll",
+                    "middle"
+            );
+
+    private Templates(File directory) {
+        if (!directory.isDirectory())
+            throw new IllegalArgumentException();
+
+        File[] childFiles = directory.listFiles();
+        try {
+            int i = 0;
+            for (File f : childFiles) {
+                if (f.getName().contains(_brickNames.get(i))) {
+                    templates[i] = new TemplateBrick(f);
+                }
+                i++;
+            }
+        } catch (IOException e) {
+
+        }
     }
 
-    public static String getNext(Type type) {
+    public String getNext(Type type) {
         if (type == Type.MAX) {
             return fillGaps(MAX_TEMP, type);
         } else if (type == Type.MIN) {
             return fillGaps(MIN_TEMP, type);
         } else if (type == Type.MIDDLE) {
-            return fillGaps(Middle.getNext(), type);
+            return fillGaps(templates[MIDDLE].getNext(), type);
         } else if (type == Type.ALL) {
-            return fillGaps(DescAll.getNext(), type);
+            return fillGaps(templates[DESCALL].getNext(), type);
         }
         // Error
         return "";
@@ -36,69 +71,52 @@ public class Templates {
         }
     }
 
-    private static String fillGaps(String s, Type type) {
+    private String fillGaps(String s, Type type) {
         StringBuilder sb = new StringBuilder(s);
         if (type == Type.MAX) {
-            replace(sb, "#rate", HighRate.getNext());
+            replace(sb, "#rate", templates[HRATE].getNext());
 
         } else if (type == Type.MIN) {
-            replace(sb, "#rate", LowRate.getNext());
+            replace(sb, "#rate", templates[LRATE].getNext());
         }
-        replace(sb, "#actors", Actors.getNext());
-        replace(sb, "#action", Actions.getNext());
+        replace(sb, "#actors", templates[ACTORS].getNext());
+        replace(sb, "#action", templates[ACTIONS].getNext());
         return sb.toString();
-    }
-
-    // Wrappers for lazy loading
-    static class Actions {
-        static final String[] values = {"ответили", "указали", "отметили", "выбрали", "проголосовали за"};
-
-        static String getNext() {
-            return values[rnd.nextInt(values.length)];
-        }
-    }
-
-    static class Actors {
-        static final String[] values = {"респондентов", "опрошенных", "принявших участие в опросе"};
-
-        static String getNext() {
-            return values[rnd.nextInt(values.length)];
-        }
-    }
-
-    static class LowRate {
-        static final String[] values = {"Только", "Всего лишь"};
-
-        static String getNext() {
-            return values[rnd.nextInt(values.length)];
-        }
-    }
-
-    static class HighRate {
-        static final String[] values = {"Большинство", "Наибольшее количество", "Больше всего"};
-
-        static String getNext() {
-            return values[rnd.nextInt(values.length)];
-        }
-    }
-
-    static class DescAll {
-        static final String[] values = {"%.1f #actor на вопрос \"%s\" #action \"%s\""};
-
-        static String getNext() {
-            return values[rnd.nextInt(values.length)];
-        }
-    }
-
-    static class Middle {
-        static final String[] values = {", %.1f%% #action \"%s\""};
-
-        static String getNext() {
-            return values[0];
-//            return values[rnd.nextInt(values.length)];
-        }
     }
 
     private static final String MIN_TEMP = "#rate %.1f%% #actors на вопрос \"%s\" #action \"%s\"";
     private static final String MAX_TEMP = "#rate #actors (%.1f%%) на вопрос \"%s\" #action \"%s\"";
+}
+
+//part of a template
+//#: Actions : {указали, выбрали и тд} - это TemplateBrick
+class TemplateBrick {
+    private static final Random rnd = new Random();
+
+    private ArrayList<String> options = new ArrayList<>();
+    private ArrayList<String> optionsUnused = new ArrayList<>();
+
+    public TemplateBrick(File file) throws IOException {
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        options.clear();
+        optionsUnused.clear();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            options.add(line);
+        }
+
+        br.close();
+        fr.close();
+    }
+
+    public String getNext() {
+        if (optionsUnused.isEmpty()) {
+            optionsUnused = new ArrayList<>(options);
+        }
+
+        return optionsUnused.remove(rnd.nextInt(optionsUnused.size()));
+    }
 }
