bd256665a1b43ffe74b2a6962ef4d6a2f37b2e81
Andrew <andpuc23@gmail.com>
2019-03-12 12:21:48 +0300

    Question_fixed

---
+++ src/main/java/com/example/sweater/Algorithms/ExcelExtract/Answer.java
@@ -0,0 +1,31 @@
+package com.example.sweater.Algorithms.ExcelExtract;
+
+public class Answer {
+    Responder responder;
+    Question question;
+    String content;
+
+    public double popularity;   //0 to 1
+
+    public Responder getResponder(){return responder;}
+
+    public Question getQuestion() {return question;}
+
+    public String getContent() {return content;}
+
+    public Answer(String answer, Question ask){
+        content = answer;
+        question = ask;
+    }
+
+    public Answer(String answer, Responder author){
+        content = answer;
+        responder = author;
+    }
+
+    public Answer(String answer, Responder author, Question ask){
+        responder = author;
+        content = answer;
+        question = ask;
+    }
+}
